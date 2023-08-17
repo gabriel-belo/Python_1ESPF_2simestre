ccb1dabea8c803873164d2bc573c5e52b639d4a4
gabrielbelo17 <gadifrb@gmail.com>
2023-08-17 11:48:56 -0300

    aula9

---
+++ aula09_17/exemplo_lista.py
@@ -0,0 +1,17 @@
+lista=[6,1,10,20,40]
+print(lista)
+print(lista[0])
+print(lista[-1])
+lista[-1]=300
+print(lista)
+print(lista[0]+ 100)
+print(max(lista))#valor máximo da lista
+print(min(lista))#valor mínimo da lista
+print(sum(lista))#soma dos valores da lista
+#calculando média da lista
+nums=[6,1,10,20,40]
+media= sum(nums)/len(nums)
+print('-=' * 15)
+nomes=["Paulo", "João", "Anna"]
+print(nomes)
+
---
+++ aula09_17/exercicios/exercicio01.py
@@ -0,0 +1,7 @@
+def frases(letras):
+    total=len(letras)
+    return total
+
+frase=str(input("Digite uma frase: "))
+batata=frases(frase)
+print(frases)
